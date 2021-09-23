47cec6bb98e348084e6a82bff2a66521987ce3ff
juulioclf <juliolima731@gmail.com>
2021-09-23 02:13:46 -0400

    adding single contact funcion

--- contatos/views.py
+++ contatos/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from .models import Contact
 
 
 def index(request):
-    return render(request, 'contatos/index.html')
+    contacts = Contact.objects.all()
+    return render(request, 'contatos/index.html', {
+        'contacts': contacts
+    })
+
+
+def search_contact(request, contact_id):
+    contact = Contact.objects.get(id=contact_id)
+    return render(request, 'contatos/search_contact.html', {
+        'contact': contact
+    })
