500466ffe95b954d9c910f5d02515e29588ad31b
juulioclf <juliolima731@gmail.com>
2021-09-23 21:13:46 -0400

    feat: raising errors when access an url nonexistent

--- contatos/views.py
+++ contatos/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 from .models import Contact
 
 
@@ -10,7 +11,11 @@ def index(request):
 
 
 def search_contact(request, contact_id):
-    contact = Contact.objects.get(id=contact_id)
+    contact = get_object_or_404(Contact, id=contact_id)
     return render(request, 'contatos/search_contact.html', {
         'contact': contact
     })
+
+
+
+
